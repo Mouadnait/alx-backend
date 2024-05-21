6090879ede284485ffe60278261f81ada70eaf87
mouadnait <moadnait6@gmail.com>
2024-05-21 13:43:00 +0100

    3. Deletion-resilient hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import Dict, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+
+        Returns:
+            List[List]: The list of rows representing the dataset.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0.
+
+        Returns:
+            Dict[int, List]: The list of rows representing the dataset starting
+            at 0.
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Retrieves a specific page of the dataset of popular baby names by
+        index.
+
+        Args:
+            index (int, optional): the index of the first item in the page to
+            retrieve.
+            If None, it defaults to the first item in the dataset.
+            page_size (int, optional): the number of items per page. Defaults
+            to 10.
+
+        Returns:
+            Dict: A dictionary containing the following key-value pairs:
+                index (int): the current start index of the return page.
+                next_index (int): the next index to query with.
+                page_size (int): the current page size
+                data (List[List]): the actual page of the dataset
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+        page_data = []
+        data_count = 0
+        next_index = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
+                page_data.append(item)
+                data_count += 1
+                continue
+            if data_count == page_size:
+                next_index = i
+                break
+        page_info = {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(page_data),
+            'data': page_data,
+        }
+        return page_info
